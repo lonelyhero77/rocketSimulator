28eb3119081e3dad69b75727cdb407344221c99b
Taesoon Kim <lonelyhero77@gmail.com>
2021-12-15 21:48:29 +0900

    Added simulating information on simulation page

--- simulator.py
+++ simulator.py
@@ -1,4 +1,7 @@
 from vpython import *
+"""
+Constants
+"""
 #mks unit
 # Re = 1
 # Rm = 273e-3
@@ -7,9 +10,6 @@ from vpython import *
 # Me = 9.37e17
 # Mm = 1.15e16
 # Rr = Rm*5
-"""
-Constants
-"""
 Re = 6.37e6
 Rm = 1.74e6
 Rem = 0.384e9
@@ -19,7 +19,7 @@ Mm = 7.35e22
 Rr = Rm*5
 
 """
-Planetary and Rocket Settings
+Planetary and Rocket Configuration
 """
 earth = sphere(radius = Re*10, color = color.white, make_trail=True, texture=textures.earth)
 earth.mass = Me
@@ -31,11 +31,9 @@ moon.mass =Mm
 moon.pos = vec(Rem, 0, 0)
 moon.vel = vec(0, 0, -1022)
 
-
 handle = cylinder( size=vector(1e8,.2e8,.2e8), color=vector(0.72,0.42,0) )
 head = box( size=vector(.2e8,.6e8,.2e8), pos=vector(1.1,0,0), color=color.gray(.6))
 hammer = compound([handle, head], make_trail=True, color= color.white, trail_color = color.green)
-# hammer.axis = vector(1,0,0)
 
 rocket = hammer
 # rocket = sphere(radius = Rr, color = color.red, make_trail=True)
@@ -43,17 +41,14 @@ rocket.mass = 4500
 rocket.pos = vec(Rem/1.2, 0, 0)
 rocket.vel = vec(0, 0, -1000)
 
-
-
 """
-System force calculation and application
+System Force Calculation and Application
 """
 system = [
     [earth, moon],
     [earth, rocket],
     [moon, rocket]
-    ]
-    
+    ]    
 planets = (earth, moon, rocket)
 
 def calculateVelocity(obj1, obj2):
@@ -65,16 +60,20 @@ def calculateVelocity(obj1, obj2):
 def calculatePosition(obj):
     obj.pos = obj.pos + obj.vel * dt
 
-
 """
-Loop
+Simulation Environment and Loop Configuration
 """
-#time settings
+# Time Settings
 dt = 100
+freq = 2000
 mt = 0
 
+# Scene Settings
+scene.title = "<b>System Simulation @ dt[sec]={}, Frequency[Hz]={}</b>".format(dt, freq)
+
+
 while True:
-    rate(2000)
+    rate(freq) # rate(frequency), halts computations until 1.0/freq seconds after the previous call to rate()
     
     for planet1, planet2 in system:
         calculateVelocity(planet1, planet2)
@@ -87,14 +86,14 @@ while True:
     mt += dt
     d = ((mt / 60) / 60 ) // 24
 
-    print("Mission Time    {}s / {}m / {}d".format(mt, mt//60, d))
+    msg = ("Mission Time    {}s / {}m / {}d".format(mt, mt//60, d))
+    print(msg)
+    scene.caption = msg
     # print(rocket.pos)
     # print(rocket.vel)
 
-    #stop when collision between rocket and earth occurred
-    if mag2(rocket.pos-earth.pos) <= (Re+Rr)**2: 
-        break
-
-    #stop when collision between rocket and moon occurred
-    if mag2(rocket.pos-moon.pos) <= (Rm+Rr)**2:
-        break
+    #Stop when collision between rocket and earth or rocket and moon has occurred
+    if mag2(rocket.pos-earth.pos) <= (Re+Rr)**2 or mag2(rocket.pos-moon.pos) <= (Rm+Rr)**2:
+        msg = ("Collision occured at Mission Time[day]: {}".format(d))
+        scene.caption = msg
+        scene.pause()
