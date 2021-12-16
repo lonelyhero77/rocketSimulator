c481d53f2c0984195690bee7f8a02018a89af90c
Taesoon Kim <lonelyhero77@gmail.com>
2021-12-17 04:51:57 +0900

    Added Init() Function Cleaned up code

--- simulator.py
+++ simulator.py
@@ -27,7 +27,6 @@ orbitDrawing = False
 # Time Settings
 dt = 10 # [sec]
 freq = 1000 # [Hz]
-initialMoonVelocity = vec(0, 0, -1022)
 warp = False
 
 """
@@ -35,21 +34,30 @@ Rocket Variables
 """
 engine = False # Rocket Engine Logic Trigger
 propulsion = False # True: Reverse, False: Forward
-initialRocketVelocity = vec(0, 0, -1395)
+initialRocketVel = vec(0, 0, -1395)
+initialRocketMass = 4500
+initialRocketPos = vec(Rem/1.1, 0, 0)
 
+"""
+Planetary Variables
+"""
+initialEarthPos = vector(0, 0, 0) # or vec(0, 0, 0)
+initialEarthVel = vector(0, 0, 0)
+initialMoonPos = vector(Rem, 0, 0)
+initialMoonVel = vec(0, 0, -1022)
 
 """
 Planetary and Rocket Configuration
 """
 earth = sphere(radius = Re*10, color = color.white, make_trail=orbitDrawing, trail_color=color.white, texture=textures.earth)
 earth.mass = Me
-earth.pos = vec(0, 0, 0)
-earth.vel = vec(0, 0, 0)
+earth.pos = initialEarthPos
+earth.vel = initialEarthVel
 
 moon = sphere(radius = Rm*10, color = color.white, make_trail=False, texture=textures.rough, retain=10)
-moon.mass =Mm
-moon.pos = vec(Rem, 0, 0)
-moon.vel = initialMoonVelocity
+moon.mass = Mm
+moon.pos = initialMoonPos
+moon.vel = initialMoonVel
 
 shaft = cylinder(size=vector(3, .3, .3), color=color.white)
 head = cone(size=vector(3.6, .3, .3), pos=vector(3,0,0), color=color.gray(.5) )
@@ -57,10 +65,9 @@ thruster = cone(size=vector(2, .2, .2), pos=vector(-1, 0, 0), color=color.gray(.
 flame = cylinder(size=vector(0.3, .15, .15), pos=vector(-1.2, 0, 0), color=color.red)
 rocket = compound([shaft, head, thruster, flame], make_trail=True, color= color.white, trail_color = color.yellow)
 rocket.size = vector(1.4e7, 0.8e7, 0.8e7)
-
-rocket.mass = 4500
-rocket.pos = vec(Rem/1.1, 0, 0)
-rocket.vel = initialRocketVelocity
+rocket.mass = initialRocketMass
+rocket.pos = initialRocketPos
+rocket.vel = initialRocketVel
 
 """
 System Force Calculation and Application
@@ -99,6 +106,20 @@ def switchPropulsion(p):
 """
 Simulation Environment and Loop Configuration
 """
+#Environment Initialization
+def init():
+    global mt, maxSpeed
+    #Set every variables to initial values
+    rocket.pos = initialRocketPos
+    rocket.vel = initialRocketVel
+    rocket.clear_trail()
+    earth.pos = initialEarthPos
+    earth.vel = initialEarthVel
+    moon.pos = initialMoonPos
+    moon.vel = initialMoonVel
+    mt = 0
+    maxSpeed = 0
+
 #Record Settings
 mt = 0
 maxSpeed = 0
@@ -115,10 +136,14 @@ def setspeed(s):
         dt= 10
         
 scene.camera.follow(rocket)
-scene.title = "<b>System Simulation @ Initial dt[sec]={}, Frequency[Hz]={}, Initial Rocket Velocity={}</b>\n".format(dt, freq, rocket.vel)
+scene.title = "<b>System Simulation @ Initial dt[sec]={}, Frequency[Hz]={}, Initial Rocket Velocity={}</b>\n\
+    ENGINE MODE | PROPULSION MODE | TIME WARP\n\
+    "\
+    .format(dt, freq, rocket.vel)
 button(text="Turn Engine On", pos=scene.title_anchor, bind=switchEngine)
 button(text="MODE: FORWARD", pos=scene.title_anchor, bind=switchPropulsion)
 button(text="WARP", pos=scene.title_anchor, bind=setspeed)
+button(text="RESET", pos=scene.title_anchor, bind=init)
 
 # Loop
 while True:
@@ -146,7 +171,7 @@ while True:
     if maxSpeed < curSpeed:
         maxSpeed = curSpeed  
 
-    msg = ("<h3>Mission Time    {}s / {}m / {}d \n |v|={spd}, |v_max|={maxspd}</h3>"\
+    msg = ("<h3>Mission Time    {}s / {}m / {}d \n |v|[m/s]={spd}, |v_max|={maxspd}</h3>"\
         .format(mt, mt//60, d, vel=rocket.vel, spd=curSpeed, maxspd=maxSpeed))
     if debug: print(msg)
     scene.caption = msg
@@ -156,8 +181,10 @@ while True:
         msg = "\n <h1>Mission completed!</h1>"
         scene.append_to_caption(msg)
         scene.pause()
+        init()
 
     if mag2(rocket.pos-moon.pos) <= (Rm+Rr)**2:
         msg = "\n <h1>Collision occured!</h1>"
         scene.append_to_caption(msg)
         scene.pause()
+        init()
